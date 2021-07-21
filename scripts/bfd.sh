745f6bc765cde2ea244778dd3100378cdabef07f
ZeroDesigner <luskyqi@outlook.com>
2021-07-21 23:10:20 +0800

    first push

---
+++ scripts/bfd.sh
@@ -0,0 +1,3 @@
+#cp /histor/gao/shiyibio/sujq/Github/RoseTTAFold/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt.tar.gz ./
+tar xfz bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt.tar.gz -C ./bfd
+
---
+++ work.sh
@@ -0,0 +1,3 @@
+source activate alphafold
+bash run_alphafold.sh -d ./scripts -o ./output/ -m model_1 -f ./example/query.fasta -t 2020-05-14
+
